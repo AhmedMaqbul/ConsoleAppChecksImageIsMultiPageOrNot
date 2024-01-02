b9e7d6a1f90d867c8c7ea52130e6015fb2c5d228
AhmedMaqbul <ahmedmaqbul.ma@gmail.com>
2024-01-02 15:57:53 +0530

    Add project files.

---
+++ ConsoleAppChecksImageIsMultiPageOrNot/Program.cs
@@ -0,0 +1,43 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppChecksImageIsMultiPageOrNot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string imagePath = "C:\\Users\\maqbul.ahmed\\Pictures\\Combined_Document 3.tiff";
+
+            try
+            {
+                using (MagickImageCollection collection = new MagickImageCollection(imagePath))
+                {
+                    if (collection.Count > 1)
+                    {
+                        Console.WriteLine($"The image '{imagePath}' is multi-page, containing {collection.Count} pages.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The image '{imagePath}' is a single-page image.");
+                    }
+                }
+            }
+            catch (MagickCorruptImageErrorException)
+            {
+                Console.WriteLine($"The image file '{imagePath}' is corrupt or not supported.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
